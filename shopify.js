951ff0b259f33a0b11f2cea3f249f6eda2e82b45
kimanthe000 <kimanthe000@gmail.com>
2026-05-28 18:14:32 -0400

    Create shopify.js

---
+++ shopify.js
@@ -0,0 +1,43 @@
+const SHOPIFY_DOMAIN = import.meta.env.VITE_SHOPIFY_STORE_DOMAIN;
+const STOREFRONT_TOKEN = import.meta.env.VITE_SHOPIFY_STOREFRONT_TOKEN;
+
+export async function createShopifyCheckout(cartItems) {
+  const query = `
+    mutation cartCreate($input: CartInput!) {
+      cartCreate(input: $input) {
+        cart {
+          checkoutUrl
+        }
+        userErrors {
+          field
+          message
+        }
+      }
+    }
+  `;
+
+  const response = await fetch(
+    `https://${SHOPIFY_DOMAIN}/api/2026-04/graphql.json`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Storefront-Access-Token": STOREFRONT_TOKEN,
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          input: {
+            lines: cartItems.map((item) => ({
+              merchandiseId: item.variantId,
+              quantity: item.quantity,
+            })),
+          },
+        },
+      }),
+    }
+  );
+
+  const { data } = await response.json();
+  return data.cartCreate.cart.checkoutUrl;
+}
